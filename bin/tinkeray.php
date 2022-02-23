5b84488661bf4c4a233edd41f2c2c70659ebc058
Jose Soto <josecanhelp@gmail.com>
2022-02-23 02:24:31 -0800

    Use AST to clean tinkeray file and auto-return the final statement

--- bin/tinkeray.php
+++ bin/tinkeray.php
@@ -1,22 +1,56 @@
 <?php
 
-class TinkerayOutputException extends Exception {}
+function generateAst($filename)
+{
+    $code = file_get_contents($filename);
+    $parser = (new PhpParser\ParserFactory())->create(PhpParser\ParserFactory::PREFER_PHP7);
+    return $parser->parse($code);
+}
 
-function getReturnFromIncludeOrFail($filename) {
-    $php = str_replace('<?php', '', file_get_contents($filename));
-    $php .= PHP_EOL.'throw new TinkerayOutputException;';
+function removeCommentLines($ast)
+{
+    $traverser = new PhpParser\NodeTraverser();
+    $traverser->addVisitor(new class () extends PhpParser\NodeVisitorAbstract {
+        public function leaveNode(PhpParser\Node $node)
+        {
+            if ($node instanceof PhpParser\Node\Stmt\Nop) {
+                return PhpParser\NodeTraverser::REMOVE_NODE;
+            }
+        }
+    });
 
-    return eval($php);
+    return $traverser->traverse($ast);
+}
+
+function enforceReturnValue($ast)
+{
+    $lastStmt = array_pop($ast);
+
+    switch ($lastStmt->getType()) {
+        case 'Stmt_Expression':
+            $ast[] = new PhpParser\Node\Stmt\Return_($lastStmt->expr);
+            break;
+        case 'Stmt_Return':
+        case 'Stmt_Echo':
+        default:
+            $ast[] = $lastStmt;
+    }
+
+    return $ast;
+}
+
+function evaluateTinkerFile($filename)
+{
+    $ast = enforceReturnValue(removeCommentLines(generateAst($filename)));
+    $executionCode = (new PhpParser\PrettyPrinter\Standard())->prettyPrint($ast);
+
+    return eval($executionCode);
 }
 
 try {
-    ray(getReturnFromIncludeOrFail(getenv('TINKERAY_APP_PATH') . '/tinkeray.php'));
+    ray(evaluateTinkerFile(getenv('TINKERAY_APP_PATH') . '/tinkeray.php'));
 } catch (Throwable $t) {
-    if ($t instanceof TinkerayOutputException) {
-        ray('Warning: No `return` detected in [tinkeray.php]!')->orange();
-    } else {
-        ray()->exception($t);
-    }
+    ray()->exception($t);
 }
 
 exit;
